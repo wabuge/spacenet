f8e342d1a7cfccc1e3b032abd77ebb1dd3e0e846
Enock Smay <notesenock@gmail.com>
2021-02-05 18:18:25 +0300

    updated
    Co-Authored-By: KENNEDY WABUGE <37111280+wabuge@users.noreply.github.com>

--- domain.php
+++ domain.php
@@ -34,25 +34,7 @@
             <![endif]-->
 
     </head>
-<!--boostrap notification -->
-
-<div role="alert" aria-live="assertive" aria-atomic="true" class="toast" data-autohide="false">
-    <div class="toast-header">
-        <svg class=" rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
-        <rect fill="#007aff" width="0%" height="100%" /></svg>
-        <strong class="mr-auto"> <font color="red" >COVID-19 </font>Message From <font color="blue"> WHO </font> </strong>
-        <small>1 mins ago</small>
-        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="toast-body">
-        <font color="red">
-            <large><strong>Hello,Our Esteemed clients, This is about COVID-19, Please USE SANITISERS, WASH YOUR HANDS FREQUENTLY, FOLLOW GOVERNMENT DIRECTIVES, KEEP SOCIAL DISTANCE(1.5M) LISTEN TO LOCAL NEWS</strong> </large>
-        </font>
-    </div>
-</div>
-
+<?php include 'inc/info-header.php'; ?>
 <body class="host_version">
 
     <!-- LOADER -->
--- index.php
+++ index.php
@@ -33,45 +33,8 @@
 
 	</head>
 
-	<!--boostrap notification -->
-
-		<div role="alert" aria-live="assertive" aria-atomic="true" class="toast" data-autohide="false">
-		    <div class="toast-header">
-		        <svg class=" rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
-		        <rect fill="#007aff" width="0%" height="100%" /></svg>
-		        <strong class="mr-auto"> <font color="red" >COVID-19 </font>Message From <font color="blue"> WHO </font> </strong>
-		        <!--script for showing time-->
-		        <large>
-		            <font color="green"><strong><script type="text/javascript"> 
-		        function display_c(){
-		        var refresh=1000; // Refresh rate in milli seconds
-		        mytime=setTimeout('display_ct()',refresh)
-		        }
-		        
-		        function display_ct() {
-		        var x = new Date()
-		        document.getElementById('ct').innerHTML = x;
-		        display_c();
-		         }
-		        </script>
-		        </head>
-		        
-		        <body onload=display_ct();>
-		        <span id='ct' ></span>
-		        </strong></large>
-		        </font>
-		        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-		        <span aria-hidden="true">&times;</span>
-		      </button>
-		    </div>
-		    <div class="toast-body">
-		        <font color="red">
-		            <large><strong>Hello,Our Esteemed clients, This is about COVID-19, Please USE SANITISERS, WASH YOUR HANDS FREQUENTLY, FOLLOW GOVERNMENT DIRECTIVES, KEEP SOCIAL DISTANCE(1.5M) LISTEN TO LOCAL NEWS</strong> </large>
-		        </font>
-		    </div>
-		</div>
-
-
+	<?php include 'inc/info-header.php'; ?>
+    
 	<body class="host_version">
 
 		<!-- LOADER -->
